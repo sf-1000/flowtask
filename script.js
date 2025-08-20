260b86b9100fcbb620ae900e4cb92c0e9a6156af
Noel Kurian <nbkurian11@gmail.com>
2025-08-19 23:36:17 -0400

    added add button and pomodoro div, need to finish to do list section (delete function) as well a pomodoro timer.

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//
+const addButton = document.getElementById('addbutton');
+const usertask = document.getElementById('taskid');
+const tasklist = document.getElementById('tasklist');
+
+
+//create an addEventListener for the add button 
+
+addButton.addEventListener('click', addTask)
+
+function addTask(){
+    const newTask = usertask.value.trim();
+
+    if (newTask != ''){
+        const taskinList = document.createElement('li');
+
+        taskinList.textContent = newTask;
+
+        tasklist.appendChild(taskinList);
+
+        usertask.value = '';
+    }
+
+}
