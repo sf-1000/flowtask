d5e161f682fce17bc63c436f50b5cc990ae707bc
Noel Kurian <nbkurian11@gmail.com>
2025-08-25 12:16:47 -0400

    worked on the backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,15 @@
 const express = require("express")
-const app = express();
+const app = express(cors);
 const port = 3000;
+
+
+
+
+app.get("/", (req, res, next) => {
+    res.send("<h1>FlowTask Home</h1>")
+})
+
+
 
 
 app.listen(port, () => {
