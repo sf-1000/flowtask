d9b6cf43c1dc32c2713ac0ad732ab252e24c986e
Noel Kurian <nbkurian11@gmail.com>
2025-08-26 12:04:44 -0400

    worked more on the backend

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,32 @@
 const express = require("express")
-const app = express(cors);
+const app = express();
 const port = 3000;
 
 
+const todos = [
+    {
+        id: 1,
+        desc: "Finish Assignment 1",
+        completed: false
+    },
+    {
+        id: 2,
+        desc: "Go to doctor's appointment",
+        completed: false
+    }
+]
 
-
-app.get("/", (req, res, next) => {
-    res.send("<h1>FlowTask Home</h1>")
-})
-
-
+//GET, POST, PUT, DELETE
 
 
 app.listen(port, () => {
-    console.log("App is listening in PORT: ", port);
+    console.log("App is listening on PORT:", port);
+});
+
+app.get("/", (req, res, next) => {
+    res.send("<h1>FlowTask Home</h1>")
+});
+
+app.get("/todos", (req, res) => {
+    res.json(todos);
 });
