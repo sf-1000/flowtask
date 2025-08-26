53530459b09e1ded37a2b57f73727d6556338959
Noel Kurian <nbkurian11@gmail.com>
2025-08-26 12:46:19 -0400

    more backend work

--- backend/index.js
+++ backend/index.js
@@ -19,14 +19,37 @@ const todos = [
 //GET, POST, PUT, DELETE
 
 
-app.listen(port, () => {
-    console.log("App is listening on PORT:", port);
-});
+
 
 app.get("/", (req, res, next) => {
-    res.send("<h1>FlowTask Home</h1>")
+    res.send("FlowTask Home")
 });
 
 app.get("/todos", (req, res) => {
     res.json(todos);
+});
+
+app.get("/todos/:id", (req, res) => {
+    console.log(req.params.id);
+    let todo = todos.filter((todo) => todo.id == req.params.id);
+    res.json(todo);
+});
+
+app.post("/todos", (req, res) => {
+    todos.push()
+    res.json([])
+})
+
+app.put("/todos/:id", (req, res) => {
+    res.json([])
+})
+
+app.delete("/todos/:id", (req, res) => {
+    res.json([])
+})
+
+
+
+app.listen(port, () => {
+    console.log("App is listening on PORT:", port);
 });
